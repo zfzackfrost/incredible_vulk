f69a3d17c2b04e3b3deb1c44caf25884d84b6043
Zachary Frost <zfzackfrost@users.noreply.github.com>
2020-09-16 11:08:12 -0400

    Add missing Doxygen comments in `AppState`

--- include/ivulk/core/app_state.hpp
+++ include/ivulk/core/app_state.hpp
@@ -10,10 +10,10 @@
 #include <ivulk/config.hpp>
 
 #include <ivulk/core/command_buffer.hpp>
+#include <ivulk/core/framebuffer.hpp>
 #include <ivulk/core/graphics_pipeline.hpp>
 #include <ivulk/core/queue_families.hpp>
 #include <ivulk/core/vma.hpp>
-#include <ivulk/core/framebuffer.hpp>
 
 #include <SDL2/SDL.h>
 #include <ivulk/vk.hpp>
@@ -43,62 +43,80 @@ namespace ivulk {
         } evt;
 
         /**
-		 * @brief Application vulkan state
+		 * @brief Vulkan application state
 		 */
         struct
         {
-            vk::Instance instance{nullptr}; ///< The Vulkan instance
-            vk::Device device{nullptr};
-            vk::PhysicalDevice physicalDevice{nullptr};
+            vk::Instance instance {nullptr};             ///< The Vulkan instance handle
+            vk::Device device {nullptr};                 ///< The Vulkan logical device handle
+            vk::PhysicalDevice physicalDevice {nullptr}; ///< The Vulkan physical device handle
 
-            VkSurfaceKHR surface                    = VK_NULL_HANDLE;
-            VkDebugUtilsMessengerEXT debugMessenger = VK_NULL_HANDLE;
-            std::vector<const char*> requiredLayers;
-            VmaAllocator allocator = VK_NULL_HANDLE;
+            VkSurfaceKHR surface                    = VK_NULL_HANDLE; ///< The Vulkan surface handle
+            VkDebugUtilsMessengerEXT debugMessenger = VK_NULL_HANDLE; ///< The Vulkan debug messenger handle
+            std::vector<const char*> requiredLayers; ///< The Vulkan layers required for creating the instance
+            VmaAllocator allocator = VK_NULL_HANDLE; ///< The VMA (Vulkan Memory Allocator) allocator handle
 
+            /**
+             * @brief Handles and state for Vulkan syncronization
+             */
             struct
             {
-                std::vector<VkSemaphore> imageAvailableSems;
-                std::vector<VkSemaphore> renderFinishedSems;
-                std::vector<VkFence> inFlightFences;
-                std::vector<VkFence> imagesInFlight;
+                std::vector<VkSemaphore>
+                    imageAvailableSems; ///< Vulkan semaphores for when a swapchain image is available
+                std::vector<VkSemaphore>
+                    renderFinishedSems;              ///< Vullkan semephores for when rendering is finshed
+                std::vector<VkFence> inFlightFences; ///< Vulkan fences for concurrent frames
+                std::vector<VkFence> imagesInFlight; ///< Vulkan fences for cuncurrent swapchain images
             } sync;
 
+            /**
+             * @brief Vulkan pipeline handles
+             */
             struct
             {
-                std::weak_ptr<GraphicsPipeline> mainGfx;
+                std::weak_ptr<GraphicsPipeline>
+                    mainGfx; ///< GraphicsPipeline that is used for rendering the final image to the swapchain
             } pipelines;
 
+            /**
+             * @brief Handles and state for Vulkan descriptor pools, sets, etc.
+             */
             struct
             {
-                VkDescriptorPool pool;
+                VkDescriptorPool pool; ///< Primary Vulkan descriptor pool
             } descriptor;
 
+            /**
+             * @brief Handles and state for Vulkan queues
+             */
             struct
             {
-                vk::Queue graphics{nullptr};
-                vk::Queue present{nullptr};
+                vk::Queue graphics {nullptr}; ///< Vulkan queue handle that supports graphics operations
+                vk::Queue present {nullptr};  ///< Vulkan queue handle that supports present operations
             } queues;
 
             /**
-			 * @brief Vulkan swap chain and related
+			 * @brief Handles and state for Vulkan swapchain
 			 */
             struct
             {
-                VkSwapchainKHR sc = VK_NULL_HANDLE;
-                std::vector<VkImage> images;
-                std::vector<VkImageView> imageViews;
-                std::vector<Framebuffer::Ptr> framebuffers;
-                VkFormat format;
-                VkExtent2D extent;
-                Image::Ptr depthImage;
+                VkSwapchainKHR sc = VK_NULL_HANDLE;         ///< Vulkan swapchain handle
+                std::vector<VkImage> images;                ///< Vulkan swapchain image handles
+                std::vector<VkImageView> imageViews;        ///< Vulkan swapchain image view handles
+                std::vector<Framebuffer::Ptr> framebuffers; ///< Vulkan swapchain framebuffer handles
+                VkFormat format;                            ///< Vulkan swapchain format
+                VkExtent2D extent;                          ///< Vulkan swapchain extent
+                Image::Ptr depthImage;                      ///< Vulkan image for swapchain depth component
             } swapChain;
 
+            /**
+             * @brief Handles and state for Vulkan command buffers, pools, etc.
+             */
             struct
             {
-                VkCommandPool gfxPool;
-                std::vector<VkCommandBuffer> gfxBuffers;
-                std::shared_ptr<CommandBuffers> renderCmdBufs;
+                VkCommandPool gfxPool;                   ///< Vulkan command pool for graphics operations
+                std::vector<VkCommandBuffer> gfxBuffers; ///< Vulkan command buffers for graphics operations
+                std::shared_ptr<CommandBuffers> renderCmdBufs; /// < Vulkan command buffers for rendering
             } cmd;
         } vk;
     };
