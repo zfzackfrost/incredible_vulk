abdac1b46f19462605ecbafc00db045f5adf3375
Zachary Frost <zfzackfrost@users.noreply.github.com>
2020-09-22 13:40:38 -0400

    Add Doxygen comments in file `ivulk/core/event.hpp`

--- include/ivulk/core/event.hpp
+++ include/ivulk/core/event.hpp
@@ -19,15 +19,25 @@
 #include <variant>
 
 namespace ivulk {
+    /**
+     * @brief Enumeration for event types
+     */
     enum class E_EventType : uint32_t
     {
-        KeyUp,
-        KeyDown,
-        MouseUp,
-        MouseDown,
-        MouseMove,
+        KeyUp,     ///< Keyboard key is released
+        KeyDown,   ///< Keyboard key is pressed
+        MouseUp,   ///< Mouse button is released
+        MouseDown, ///< Mouse button is pressed
+        MouseMove, ///< Mouse pointer is moved
     };
+
+    /**
+     * @brief Namespace containing keycode constants
+     */
     namespace E_KeyCode {
+        /**
+         * @brief The type of each keycode constant
+         */
         using Type = SDL_Keycode;
 
         // Number row
@@ -144,7 +154,13 @@ namespace ivulk {
 
     }; // namespace E_KeyCode
 
+    /**
+     * @brief Namespace containing mouse button constants
+     */
     namespace E_MouseButton {
+        /**
+         * @brief The type of each mouse button constant
+         */
         using Type = Uint8;
 
         constexpr Type BtnLeft   = SDL_BUTTON_LEFT;
@@ -152,35 +168,65 @@ namespace ivulk {
         constexpr Type BtnRight  = SDL_BUTTON_RIGHT;
     } // namespace E_MouseButton
 
+    /**
+     * @brief Data for a key up or key down event
+     */
     struct KeyEvent
     {
-        bool bRepeat : 1;
-        bool bIsDown : 1;
-        E_KeyCode::Type keycode;
+        bool bRepeat : 1;        ///< Is this a repeat event? (i.e. The key is held down)
+        bool bIsDown : 1;        ///< Is this a key down event?
+        E_KeyCode::Type keycode; ///< The key code for this event
     };
 
+    /**
+     * @brief Data for a mouse up or mouse down event
+     */
     struct MouseButtonEvent
     {
-        bool bIsDown : 1;
-        E_MouseButton::Type button;
+        bool bIsDown : 1;           ///< Is this a mouse down event?
+        E_MouseButton::Type button; ///< The mouse button for this event
     };
+
+    /**
+     * @brief Data for a mouse move event
+     */
     struct MouseMoveEvent
     {
-        glm::ivec2 mousePos;
-        glm::ivec2 mouseDelta;
+        glm::ivec2 mousePos;   ///< The current mouse position
+        glm::ivec2 mouseDelta; ///< The change in mouse position
     };
 
+    /**
+     * @brief STL `variant` to accept any event data.
+     */
     using AnyEventData = std::variant<KeyEvent, MouseButtonEvent, MouseMoveEvent>;
 
+    /**
+     * @brief Data describing a user input event.
+     */
     struct Event
     {
-        E_EventType type;
-        AnyEventData data;
+        E_EventType type;  ///< The event type
+        AnyEventData data; ///< The event data
 
+        /**
+         * @brief Get the event data as a `KeyEvent`
+         */
         KeyEvent assumeKeyEvent() { return std::get<KeyEvent>(data); }
+
+        /**
+         * @brief Get the event data as a `MouseButtonEvent`
+         */
         MouseButtonEvent assumeMouseBtnEvent() { return std::get<MouseButtonEvent>(data); }
+
+        /**
+         * @brief Get the event data as a `MouseMoveEvent`
+         */
         MouseMoveEvent assumeMouseMoveEvent() { return std::get<MouseMoveEvent>(data); }
 
+        /**
+         * @brief Make an `Event` structure for a key up or key down event
+         */
         static inline Event makeKeyEvent(KeyEvent data)
         {
             return Event {
@@ -188,6 +234,10 @@ namespace ivulk {
                 .data = data,
             };
         }
+
+        /**
+         * @brief Make an `Event` structure for a mouse up or mouse down event
+         */
         static inline Event makeMouseBtnEvent(MouseButtonEvent data)
         {
             return Event {
@@ -195,6 +245,10 @@ namespace ivulk {
                 .data = data,
             };
         }
+
+        /**
+         * @brief Make an `Event` structure for a mouse move
+         */
         static inline Event makeMouseMoveEvent(MouseMoveEvent data)
         {
             return Event {
@@ -203,18 +257,54 @@ namespace ivulk {
             };
         }
 
+        /**
+         * @brief Make an `Event` structure from an SDL event
+         *
+         * @return Event structure for `evt` on success, otherwise `std::nullopt`.
+         */
         static std::optional<Event> fromSDLEvent(SDL_Event evt);
     };
 
+    /**
+     * @brief Static class that contains the event queue and manages input callbacks
+     */
     class EventManager final
     {
     public:
+        /**
+         * @brief The input callback type
+         */
         using Callback = std::function<void(Event)>;
+
+        /**
+         * @brief Register a callback
+         *
+         * @param eventType the type of event to register the callback for
+         * @param cb The callback to register
+         */
         static void addCallback(E_EventType eventType, const Callback& cb);
 
+        /**
+         * @brief Add an `Event` to the end of the queue.
+         *
+         * @param evt The event to add
+         */
         static void pushEvent(const Event evt);
+        /**
+         * @brief Add an `SDL_Event` to the end of the queue.
+         *
+         * @param sdlEvt The event to add
+         */
         static void pushEvent(const SDL_Event sdlEvt);
+
+        /**
+         * @brief Remove the first event from the queue and execute matching callbacks
+         */
         static void popEvent();
+
+        /**
+         * @brief Call `popEvent()` until the queue is empty.
+         */
         static void processAllEvents();
 
     private:
