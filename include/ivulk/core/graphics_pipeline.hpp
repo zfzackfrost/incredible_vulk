c3d4554f3aca6ff58daddc9349d935fc3481627d
Zachary Frost <zfzackfrost@users.noreply.github.com>
2020-09-22 14:22:24 -0400

    Add Doxygen comments for `GraphicsPipeline` and related.

--- include/ivulk/core/graphics_pipeline.hpp
+++ include/ivulk/core/graphics_pipeline.hpp
@@ -23,27 +23,40 @@
 #include <boost/filesystem.hpp>
 
 namespace ivulk {
+    /**
+     * @brief Information for initializing a GraphicsPipeline resource
+     */
     struct GraphicsPipelineInfo final
     {
-        PipelineVertexInfo vertex;
+        PipelineVertexInfo vertex; ///< The vertex format for the graphics pipeline
 
-        struct
+        /**
+         * @brief The paths to load SPIR-V shaders from
+         */
+        struct shaderPath
         {
-            std::optional<boost::filesystem::path> vert = {};
-            std::optional<boost::filesystem::path> frag = {};
-            std::optional<boost::filesystem::path> tese = {};
-            std::optional<boost::filesystem::path> tesc = {};
-            std::optional<boost::filesystem::path> geom = {};
-            std::optional<boost::filesystem::path> comp = {};
+            std::optional<boost::filesystem::path> vert = {}; ///< Vertex shader path
+            std::optional<boost::filesystem::path> frag = {}; ///< Fragment shader path
+            std::optional<boost::filesystem::path> tese = {}; ///< Tesselation evaluation shader path
+            std::optional<boost::filesystem::path> tesc = {}; ///< Tesselation control shader path
+            std::optional<boost::filesystem::path> geom = {}; ///< Geometry shader path
+            std::optional<boost::filesystem::path> comp = {}; ///< Compute shader path
         } shaderPath;
 
-        struct
+
+        /**
+         * @brief Pipeline descriptor bindings
+         */
+        struct descriptor
         {
-            std::vector<PipelineUniformBufferBinding> uboBindings = {};
-            std::vector<PipelineTextureBinding> textureBindings   = {};
+            std::vector<PipelineUniformBufferBinding> uboBindings = {}; ///< Descriptor bindings for uniform buffers
+            std::vector<PipelineTextureBinding> textureBindings   = {}; ///< Descriptor bindings for textures
         } descriptor;
     };
 
+    /**
+     * @brief A memory-managed resource for a Vulkan graphics pipeline
+     */
     class GraphicsPipeline : public VulkanResource<GraphicsPipeline,
                                                    GraphicsPipelineInfo,
                                                    VkPipeline,
@@ -53,15 +66,50 @@ namespace ivulk {
                                                    std::vector<VkDescriptorSet>>
     {
     public:
+        /**
+         * @brief Get the Vulkan pipeline handle
+         */
         VkPipeline getPipeline() { return getHandleAt<0>(); }
+        
+        /**
+         * @brief Get the Vulkan render pass handle
+         */
         VkRenderPass getRenderPass() { return getHandleAt<1>(); }
+        
+        /**
+         * @brief Get the Vulkan pipeline layout handle
+         */
         VkPipelineLayout getPipelineLayout() { return getHandleAt<2>(); }
+        
+        /**
+         * @brief Get the Vulkan descriptor set layout handle
+         */
         VkDescriptorSetLayout getDescriptorSetLayout() { return getHandleAt<3>(); }
+        
+        /**
+         * @brief Get the STL vector of Vulkan descriptor set handles
+         */
         std::vector<VkDescriptorSet> getDescriptorSets() { return getHandleAt<4>(); }
+
+        /**
+         * @brief Get a Vulkan descriptor set by index
+         *
+         * @param i The index of the descriptor set to get
+         */
         VkDescriptorSet getDescriptorSetAt(std::size_t i) { return getDescriptorSets().at(i); }
 
+        /**
+         * @brief Get an STL vector of color attachment indices
+         */
         std::vector<uint32_t> getColorAttIndices() { return m_colorAttIndices; }
 
+        /**
+         * @brief Create a new graphics pipeline, and store it in this resource.
+         *
+         * This destroys the current pipline before creating the new one.
+         *
+         * @param info The parameters to use to create the new pipeline.
+         */
         void recreate(GraphicsPipelineInfo info);
 
     private:
