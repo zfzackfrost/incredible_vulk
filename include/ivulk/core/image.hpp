d4d871c6e65ecd39e4c36ac3ec90d8c45d46eea7
Zachary Frost <zfzackfrost@users.noreply.github.com>
2020-09-22 15:21:43 -0400

    Add Doxygen comments for `Image` and `VulkanResource`

--- include/ivulk/core/image.hpp
+++ include/ivulk/core/image.hpp
@@ -19,31 +19,56 @@
 #include <optional>
 
 namespace ivulk {
+    /**
+     * @brief Information for initializing an Image resource
+     */
     struct ImageInfo final
     {
-        struct
+        /**
+         * @brief Settings for loading images from the filesystem.
+         */
+        struct load
         {
-            bool bEnable = false;
-            boost::filesystem::path path;
-            bool bSrgb = true;
+            bool bEnable = false;         ///< Set this to true to load the image from the filesystem
+            boost::filesystem::path path; ///< The file path to load the image from
+            bool bSrgb
+                = true; ///< If true (default), use an sRGB format when loading the image. Otherwise use a normalized format.
         } load;
 
-        VkImageTiling tiling      = VK_IMAGE_TILING_OPTIMAL;
-        VkImageUsageFlags usage   = VK_IMAGE_USAGE_SAMPLED_BIT;
-        VkSharingMode sharingMode = VK_SHARING_MODE_EXCLUSIVE;
-        VmaMemoryUsage memoryMode = E_MemoryMode::GpuOnly;
-        VkFormat format           = VK_FORMAT_R8G8B8A8_SRGB;
-        VkExtent3D extent {};
-        VkImageLayout layout      = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
-        VkImageAspectFlags aspect = VK_IMAGE_ASPECT_COLOR_BIT;
+        VkImageTiling tiling      = VK_IMAGE_TILING_OPTIMAL;    ///< Vulkan image tiling setting
+        VkImageUsageFlags usage   = VK_IMAGE_USAGE_SAMPLED_BIT; ///< Vulkan image usage flags
+        VkSharingMode sharingMode = VK_SHARING_MODE_EXCLUSIVE;  ///< Vulkan image sharing mode
+        VmaMemoryUsage memoryMode = E_MemoryMode::GpuOnly;      ///< Memory usage mode
+        VkFormat format = VK_FORMAT_R8G8B8A8_SRGB; ///< Vulkan image format, if not loading from filesystem.
+        VkExtent3D extent {};                      ///< The image extent
+        VkImageLayout layout      = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL; ///< Vulkan image layout
+        VkImageAspectFlags aspect = VK_IMAGE_ASPECT_COLOR_BIT;                ///< Vulkan image aspect flags
     };
 
+    /**
+     * @brief A memory-managed resource for a Vulkan image and image view
+     */
     class Image : public VulkanResource<Image, ImageInfo, VkImage, VmaAllocation, VkImageView>
     {
     public:
+        /**
+         * @brief Get the Vulkan image handle
+         */
         VkImage getImage() { return getHandleAt<0>(); }
+
+        /**
+         * @brief Get the VMA allocation information.
+         */
         VmaAllocation getAllocation() { return getHandleAt<1>(); }
+
+        /**
+         * @brief Get the Vulkan image view handle.
+         */
         VkImageView getImageView() { return getHandleAt<2>(); }
+
+        /**
+         * @brief Get the Vulkan image format.
+         */
         VkFormat getFormat() { return m_format; }
 
     private:
--- include/ivulk/core/vulkan_resource.hpp
+++ include/ivulk/core/vulkan_resource.hpp
@@ -15,9 +15,23 @@
 #include <memory>
 
 namespace ivulk {
+    /**
+     * @brief Empty information for initializing a resource.
+     */
     struct NullResourceInfo final
     { };
 
+    /**
+     * @brief Base class template for memory-managed wrapper classes for Vulkan resources
+     *
+     * @tparam Derived A derived type (for CRTP)
+     * @tparam CreateInfo The structure used to provide initialization 
+     *                    information for the derived type
+     * @tparam HandleTypes Parameter pack where each type corresponds to 
+     *                     some arbitrary data stored in a tuple in the 
+     *                     resource. The data is primarily Vulkan handles,
+     *                     but any type is allowed.
+     */
     template <typename Derived, typename CreateInfo, typename... HandleTypes>
     class VulkanResource
     {
@@ -35,13 +49,35 @@ namespace ivulk {
         using base_t    = VulkanResource<Derived, CreateInfo, HandleTypes...>;
         using handles_t = std::tuple<HandleTypes...>;
 
+        /**
+         * @brief Constructor
+         *
+         * @param device The Vulkan device associated with this resource
+         * @param h The tuple containing the handle and/or other data for this resource
+         */
         VulkanResource(VkDevice device, const std::tuple<HandleTypes...>&& h)
             : m_device(device)
             , handles(h)
         { }
 
+        /**
+         * @brief Virtual destructor
+         *
+         * Calls `destroy()`
+         */
         virtual ~VulkanResource() { destroy(); }
 
+
+        /**
+         * @brief Destroys the resource.
+         *
+         *
+         * If `destroy()` has already been called, this has no effect.
+         *
+         * @note
+         * Calls a non-static member function `Derived::destroyImpl()` on
+         * `this`. 
+         */
         void destroy()
         {
             if (m_destroyed)
@@ -50,17 +86,34 @@ namespace ivulk {
             m_destroyed = true;
         }
 
+        /**
+         * @brief Gets a value from the handle/data tuple by index
+         *
+         * @tparam I The index of the handle/data to get
+         */
         template <std::size_t I>
         auto getHandleAt()
         {
             return std::get<I>(handles);
         }
 
+        /**
+         * @brief Create a new instance from handles
+         *
+         * @param device The device to associate with the new resource
+         * @param args The values to fill the handles/data tuple with
+         */
         static std::shared_ptr<Derived> fromHandles(VkDevice device, HandleTypes... args)
         {
             return std::shared_ptr<Derived>(new Derived(device, args...));
         }
 
+        /**
+         * @brief Create a new instance using initialization information
+         *
+         * @param device The device to associate with the new resource
+         * @param createInfo The initialization info to create a new instance with
+         */
         static std::shared_ptr<Derived> create(VkDevice device, const CreateInfo& createInfo)
         {
             return std::shared_ptr<Derived>(Derived::createImpl(device, createInfo));
